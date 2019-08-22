af928fe15fe442e578b5e5486d7da02d58ec3d29
jimmyyhsu <49001256+jimmyyhsu@users.noreply.github.com>
2019-08-21 22:29:42 -0700

    Created for loop to print pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 var repository = [
-  {name: 'Bulbasaur', index: 1, height: 0.7, weight: 6.9, types: ['grass','poison']},
-  {name: 'Ivysaur', index: 2, height: 1.0, weight: 13.0, types: ['grass','poison']},
-  {name: 'Venusaur', index: 3, height: 2.0, weight: 100.0, types: ['grass','poison']},
+  {
+    name: 'Bulbasaur',
+    index: 1,
+    height: 0.7,
+    weight: 6.9,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Ivysaur',
+    index: 2,
+    height: 1.0,
+    weight: 13.0,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Venusaur',
+    index: 3,
+    height: 2.0,
+    weight: 100.0,
+    types: ['grass','poison']
+  },
 ];
+
+for (items in repository) {
+  document.write(repository[items].name + ' (height: ' + repository[items].height + 'm)<br>');
+}
