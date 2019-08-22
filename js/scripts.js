a183d58b6d631105fc43d1e52bc7f4de6ed75471
jimmyyhsu <49001256+jimmyyhsu@users.noreply.github.com>
2019-08-21 21:37:06 -0700

    Added pokemon data to repository array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-var repository = [];
+var repository = [
+  {name: 'Bulbasaur', index: 1, height: 0.7, weight: 6.9, types: ['grass','poison']},
+  {name: 'Ivysaur', index: 2, height: 1.0, weight: 13.0, types: ['grass','poison']},
+  {name: 'Venusaur', index: 3, height: 2.0, weight: 100.0, types: ['grass','poison']},
+];
