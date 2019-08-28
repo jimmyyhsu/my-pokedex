e75931506d241d6483eecfd9cf19206b2eb8133d
jimmyyhsu <49001256+jimmyyhsu@users.noreply.github.com>
2019-08-28 12:56:42 -0700

    Assigned IIFE to pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,43 @@
-var repository = [
-  {
-    name: 'Bulbasaur',
-    index: 1,
-    height: 0.7,
-    weight: 6.9,
-    types: ['grass','poison']
-  },
-  {
-    name: 'Ivysaur',
-    index: 2,
-    height: 1.0,
-    weight: 13.0,
-    types: ['grass','poison']
-  },
-  {
-    name: 'Venusaur',
-    index: 3,
-    height: 2.0,
-    weight: 100.0,
-    types: ['grass','poison']
-  },
-];
+var pokemonRepository = (function(){
+  var repository = [
+    {
+      name: 'Bulbasaur',
+      index: 1,
+      height: 0.7,
+      weight: 6.9,
+      types: ['grass','poison']
+    },
+    {
+      name: 'Ivysaur',
+      index: 2,
+      height: 1.0,
+      weight: 13.0,
+      types: ['grass','poison']
+    },
+    {
+      name: 'Venusaur',
+      index: 3,
+      height: 2.0,
+      weight: 100.0,
+      types: ['grass','poison']
+    }
+  ];
+
+  //Function to add new Pokemon data
+  function add(pokemon){
+    repository.push(pokemon);
+  }
+
+  //Function to pull all Pokemon data
+  function getAll(){
+    return repository;
+  }
+
+  return{
+    add: add,
+    getAll: getAll
+  };
+})();
 
 //for (items in repository) {
   //if (repository[items].height > 1) {
@@ -30,7 +47,7 @@ var repository = [
   //}
 //}
 
-repository.forEach(function(currentItems){
+pokemonRepository.forEach(function(currentItems){
   Object.keys(currentItems).forEach(function(property){
     document.write(property + ': ' + currentItems[property] + '<br>');
   });
